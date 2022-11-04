a5fee8d89e942cbe7fbca60a943c2a051ccaf47c
“Mukesh <mukesh060519@gmail.com>
2022-11-04 23:29:53 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Box bgcolor={"background.default"} color={"text.primary"}>
         <Navbar />
         <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar setMode={setMode} mode={mode}/>
+          <Sidebar setMode={setMode} mode={mode} />
           <Feed />
           <Rightbar />
         </Stack>
